6f4562103f4d6b11ebe4fda5757f7de0d03a8502
Mohamed Mamdouh <triplem.coder@gmail.com>
2019-05-27 07:37:22 +0200

    Design screens

---
+++ Login Form/Controller/RegisterVC.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterVC.swift
+//  Login Form
+//
+//  Created by Mohamed Mamdouh on 5/26/19.
+//  Copyright © 2019 Mohamed Mamdouh. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
